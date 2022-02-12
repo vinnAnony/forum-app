f5bca4ebd57c5f6218f9ba3dd3eb507d5de4889e
Vmbugua <vmbugua@cytonn.com>
2022-02-12 12:57:06 +0300

    Lesson 2

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,10 +10,20 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ThreadsTest extends TestCase
 {
     /** @test */
-    public function user_ca(){
+    public function a_user_can_view_all_threads()
+    {
         $thread = factory('App\Thread')->create();
 
         $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
         $response->assertSee($thread->title);
     }
 }
